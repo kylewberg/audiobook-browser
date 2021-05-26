abff1854647eb76a221836360bd9cc3a883205d9
Kyle Berg <kylewberg@gmail.com>
2021-05-26 14:37:14 -0700

    added Netlify config file

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,6 +4,7 @@
             <h1 class="text-6xl font-bold">About this site</h1>
             <p class="text-xl mt-4">This site is an application to browse the <a href="https://librivox.org/" class="text-blue-900 underline hover:no-underline" target="_blank">LibriVox catalog</a>.</p>
             <p class="text-xl mt-4">It is built using <a href="https://vuejs.org/" class="text-blue-900 underline hover:no-underline" target="_blank">Vue.js</a> and uses the free <a href="https://librivox.org/api/info" class="text-blue-900 underline hover:no-underline" target="_blank">LibriVox API</a>. It is styled using <a href="https://tailwindcss.com/" class="text-blue-900 underline hover:no-underline" target="_blank">Tailwind CSS</a>.</p>
+            <p class="text-xl mt-4">The codebase can be <a href="https://github.com/kylewberg/audiobook-browser" class="text-blue-900 underline hover:no-underline" target="_blank">viewed on GitHub</a>.</p>
         </section>
     </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <section class="flex justify-center items-center text-center" style="padding: 20% 0;">
+        <section class="flex justify-center items-center text-center" style="padding: 14% 0;">
             <div class="w-full max-w-screen-md">
                 <h1 class="text-4xl lg:text-7xl font-bold">Browse Audiobooks in the Public Domain</h1>
                 <p class="mt-8 text-lg lg:text-xl">This site leverages the awesome LibriVox audiobook catalog. LibriVox audiobooks are free for anyone to listen, please consider making a <a class="text-blue-900 underline hover:no-underline" href="https://librivox.org/pages/donate-to-librivox/" target="_blank">donation</a> or <a class="text-blue-900 underline hover:no-underline" href="https://librivox.org/pages/volunteer-for-librivox/" target="_blank">volunteering</a>.</p>
